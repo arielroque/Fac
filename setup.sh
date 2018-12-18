c8300a03ce682ab562cec6cbb66637bb3d279766
ariel.luz <ariel.luz@virtus.ufcg.edu.br>
2018-12-18 15:56:16 -0300

    Finished features

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ function addUrlMozilaNavegator(){
 	URL=$(whiptail --title "Select URL" --inputbox "Enter the desire URL:" 10 60 3>&1 1>&2 2>&3)
 	existStatus=$?
 	if [ $existStatus = 0 ]; then
-	 	echo "firefox $URL" >> values.sh
+	 	echo "firefox $URL" >> ~/fac1/$alias.sh
 	else
 	    echo "You Choose Cancel."
 	fi
@@ -16,7 +16,7 @@ function addUrlGoogleNavegator(){
 	exitStatus=$?
 
 	if [ $exitStatus = 0 ]; then
-    	echo "google-chrome $URL" >> values.sh
+    	echo "google-chrome $URL" >> ~/fac1/$alias.sh
 	else
     	echo "You choose Cancel."
 	fi
@@ -34,13 +34,49 @@ function startSetup (){
 			addUrlGoogleNavegator;;
 		2) 
 			addUrlMozilaNavegator;;
+
+		*) whiptail --title  "Finish fac" --msgbox "Alias succesfuly saved" 8 78
+		   
+		   echo " alias $alias='source ~/fac1/$alias.sh'" >> ~/.bashrc
+
+		   exit		
 	esac
+
+    exitStatus=$?
+	
+	if [ $exitStatus = 0 ]; then
+
+	     echo "teste"
+	
+	else 
+
+	    exit
+	fi	
+}
+
+function createAlias(){
+
+	alias=$(whiptail --title "Alias Name" --inputbox "Enter the alias name:" 10 60 3>&1 1>&2 2>&3)
+	exitStatus=$?
+
+	if [ $exitStatus = 1 ]; then
+	    #improve the user's exit 
+	    exit
+	fi
 }
 
 if ( whiptail --title "Fac Wizard" --yes-button "Ok" --no-button "Cancel"  --yesno "Welcome to the Fast Automatization Command (fac). Choose Ok to <continue> or <cancel> to exit." 10 60 ); then
-	
+    
+	createAlias
+
 	while [ true ]; do
+
+     if [ ! -d ~/fac1 ]; then	    
+		 mkdir ~/fac1
+	 fi
+
      startSetup
+
     done
 else
 	exit
--- values.sh
+++
@@ -1,4 +0,0 @@
-google-chrome www.google.com
-firefox www.google.com
-google-chrome www.hotmail.com
-firefox www.gmail.com
